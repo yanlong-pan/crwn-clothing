2c2770f50546554771f04d26440ffa5f35c2927f
pyl <a522723936@gmail.com>
2019-12-10 18:09:40 +1100

    break our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.copmponent';
+
+
+const HomePage = () => (
+    <div className='homepage'> 
+        <Directory />        
+    </div>
+)
+
+export default HomePage;
