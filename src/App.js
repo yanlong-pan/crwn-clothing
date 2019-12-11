83e780e82ff347c74401b01d0bfd1a29809cf62f
pyl <a522723936@gmail.com>
2019-12-11 21:18:49 +1100

    updating app with a sop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1> Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/shop/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
     </Switch>
     </div>
   );
