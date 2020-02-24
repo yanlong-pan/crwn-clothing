8ab64c05a871dc90b73564e6ebbbad605c9f3637
pyl <a522723936@gmail.com>
2020-02-24 21:00:43 +1100

    moved directory and shop data into its own reducer and created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/directory/directory.copmponent.jsx
+++ src/components/directory/directory.copmponent.jsx
@@ -1,26 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
+import {selectDirectorySections} from '../../redux/directory/directory.selectors'
 import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss'
-import SECTION_DATA from './sections.data'
-
-class Directory extends Component {
-    constructor(){
-        super();
-        this.state = {
-            sections:SECTION_DATA
+const Directory = ({sections}) => (
+    <div className='directory-menu'>
+        {
+            sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}/>
+            ))
         }
-    }
-
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({id, ...otherSectionProps}) => (
-                        <MenuItem key={id} {...otherSectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
-export default Directory;
+    </div>
+);
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,17 @@
+import {createSelector} from 'reselect'
+
+const selectShop = state => state.shop
+
+export const selectShopCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+)
+export const selectShopCollectionsForPreview = createSelector(
+    [selectShopCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+)
