fb8742fc0de75307df45714547b95aba40388955
pyl <a522723936@gmail.com>
2020-02-28 20:47:46 +1100

    added a one time data auto-fill into firebase DB functionality which will be removed later

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
-import {auth, createUserProfileDocument}from './firebase/firebase.utils' 
+import {auth, createUserProfileDocument, addCollectionAndDocuments}from './firebase/firebase.utils' 
 import {setCurrentUser} from './redux/user/user.actions'
 import {selectCurrentUser} from './redux/user/user.selectors'
+
+import { selectShopCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   constructor(){
@@ -23,7 +25,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    const {setCurrentUser} = this.props
+    const {setCurrentUser, collectionsArray} = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
@@ -36,6 +38,7 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
     });
   }
       
@@ -60,7 +63,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectShopCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
