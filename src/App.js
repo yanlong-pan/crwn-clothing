ab6a0d63a0e96d83e8d2622d29c2a1c8e50451c2
pyl <a522723936@gmail.com>
2020-02-29 22:14:00 +1100

    Implement code that pulls in shop data from firestore and stores it into the shop reducer from our shop page. Also, included our WithSpinner HOC

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
-import {auth, createUserProfileDocument, addCollectionAndDocuments}from './firebase/firebase.utils' 
+import {auth, createUserProfileDocument}from './firebase/firebase.utils' 
 import {setCurrentUser} from './redux/user/user.actions'
 import {selectCurrentUser} from './redux/user/user.selectors'
 
-import { selectShopCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   constructor(){
@@ -25,7 +24,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    const {setCurrentUser, collectionsArray} = this.props
+    const {setCurrentUser} = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
@@ -38,7 +37,6 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
-      addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
     });
   }
       
@@ -64,7 +62,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArray: selectShopCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -47,6 +47,22 @@ const config = {
     return await batch.commit()
   }
 
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollections = collections.docs.map( doc => {
+      const {title, items} = doc.data()
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      }
+    })
+    return transformedCollections.reduce((accumulator, collection)=> {
+      accumulator[collection.title.toLowerCase()] = collection
+      return accumulator
+    }, {})
+  }
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
