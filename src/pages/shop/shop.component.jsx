270fd902bb66cea95fe370f746fe3e4d2946db42
pyl <a522723936@gmail.com>
2020-03-03 21:05:26 +1100

    added redux-saga to replace redux-thunk. And writed sagas to replace fetchCollectionsStartAsync

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,13 +3,13 @@ import {connect} from 'react-redux'
 import {Route} from 'react-router-dom'
 import CollecitonsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container.jsx'
-import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions'
+import {fetchCollectionsStart} from '../../redux/shop/shop.actions'
 
 
 class ShopPage extends React.Component {
     componentDidMount(){
-        const {fetchCollectionsStartAsync} = this.props
-        fetchCollectionsStartAsync()
+        const {fetchCollectionsStart} = this.props
+        fetchCollectionsStart()
     }
     render() {
         const {match} = this.props
@@ -23,6 +23,6 @@ class ShopPage extends React.Component {
 } 
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 export default connect(null, mapDispatchToProps)(ShopPage);
